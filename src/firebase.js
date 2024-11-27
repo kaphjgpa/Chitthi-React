452089bdcbdf98fe0cdaed42847e461defa78a94
Surendra singh <surendrasinghc80@gmail.com>
2024-11-28 03:07:17 +0530

    fix .env file

--- src/firebase.js
+++ src/firebase.js
@@ -24,14 +24,24 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCCFPm_me39f48H0jZLrByu6QSgMTPIV0g",
+//   authDomain: "project-x-e3c38.firebaseapp.com",
+//   projectId: "project-x-e3c38",
+//   storageBucket: "project-x-e3c38.appspot.com",
+//   messagingSenderId: "908665978885",
+//   appId: "1:908665978885:web:6963083df0a10afcb21c6c",
+//   measurementId: "G-EQBQF885BY",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCCFPm_me39f48H0jZLrByu6QSgMTPIV0g",
-  authDomain: "project-x-e3c38.firebaseapp.com",
-  projectId: "project-x-e3c38",
-  storageBucket: "project-x-e3c38.appspot.com",
-  messagingSenderId: "908665978885",
-  appId: "1:908665978885:web:6963083df0a10afcb21c6c",
-  measurementId: "G-EQBQF885BY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
